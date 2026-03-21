08e0dd9272d885f5a7204eef2f4b29c90e0de551
René Kreijveld <email@renekreijveld.nl>
2026-03-21 20:37:58 +0100

    Update password prompt message for clarity in ddev-joomla-updater script

--- updater/ddev-joomla-updater.sh
+++ updater/ddev-joomla-updater.sh
@@ -75,7 +75,7 @@ start() {
         exit 0
     fi
 
-    read -s -p "Input your password, this is needed for installing the scripts in : " PASSWORD
+    read -s -p "Input your password, this is needed to install the scripts at ${SCRIPTS_DEST}: " PASSWORD
     echo ""
 
     # Validate the password
